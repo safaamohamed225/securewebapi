cf8b1e280879564a79f501bb87809fe0f052d866
Safa Mohamed <safaa.m.ibrahem@gmail.com>
2026-02-03 16:17:11 +0200

    Seed Database With Needed Roles

---
+++ SecureWebAPI/Migrations/20260203140613_SeedingData.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SecureWebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedingData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+
+                table: "AspNetRoles",
+                columns: new[] { "Id", "Name", "NormalizedName", "ConcurrencyStamp" },
+                values: new object[,]
+                {
+                    { Guid.NewGuid().ToString(), "Admin", "Admin".ToUpper(), Guid.NewGuid().ToString() },
+                    { Guid.NewGuid().ToString(), "User", "User".ToUpper(), Guid.NewGuid().ToString() }
+                }
+                );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Name",
+                keyValues: new object[]
+                {
+                    "Admin",
+                    "User"
+                });
+        }
+    }
+}
